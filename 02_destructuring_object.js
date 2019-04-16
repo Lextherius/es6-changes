29da08e13b9e27b8e0313add2b24e17d072dda49
Lextherius <alex@lexethos.com>
2019-04-16 18:50:08 +0800

    Destructuring Objects
    Write shorthand code and reassign value
    
    Result:
    Howson Lex

---
+++ 02_destructuring_object.js
@@ -0,0 +1,11 @@
+const personalInformation = {
+    firstName : 'Howson',
+    lastName : 'Lex',
+    city : 'San Franciso',
+    state : 'California',
+    zipcode : 1234567
+}
+
+const {firstName, lastName} = personalInformation;
+
+console.log(`${firstName} ${lastName}`);
