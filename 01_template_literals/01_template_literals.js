9cb2e0b6bd65a13cbde7de13bc42e26ce2f82c93
Lextherius <alex@lexethos.com>
2019-04-16 18:27:37 +0800

    Template Literals
    New line with good of JS
    
    Results:
    Hello
    World

--- 01_template_literals/01_template_literals.js
+++ 01_template_literals/01_template_literals.js
@@ -3,6 +3,9 @@ let word2 = 'Lex';
 let num1 = 9;
 let num2 = 3;
 
-const fullName = `${num1 + num2} ${word2}`;
+// const fullName = `${num1 + num2} ${word2}`;
 
-console.log(fullName);
+// console.log(fullName);
+
+let example = 'Hello\n' +  'World';
+console.log(example);
