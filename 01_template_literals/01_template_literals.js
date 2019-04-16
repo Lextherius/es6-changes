f9e271415eb9ef6a0afd84e506d3a938f754c260
Lextherius <alex@lexethos.com>
2019-04-16 18:23:25 +0800

    Template Literals
    We can even do arithmetic calculations.
    
    Result : 12 Lex

--- 01_template_literals/01_template_literals.js
+++ 01_template_literals/01_template_literals.js
@@ -1,6 +1,8 @@
 let word1 = 'Howson';
 let word2 = 'Lex';
+let num1 = 9;
+let num2 = 3;
 
-const fullName = `${word1} ${word2}`;
+const fullName = `${num1 + num2} ${word2}`;
 
 console.log(fullName);
