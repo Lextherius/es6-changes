d805462b80c2aa49e6a83b54b7651e4c020cdf72
Lextherius <alex@lexethos.com>
2019-04-17 14:54:08 +0800

    Object literal challenge
    Make use of
    1. object destructuring
    2. object literal
    3. string/template literal
    
    result:
    Austin, Texas, United States

--- 05_object_literal_challenge/05_object_literal_challenge.js
+++ 05_object_literal_challenge/05_object_literal_challenge.js
@@ -1,9 +1,12 @@
-function addressMaker(address){
+function addressMaker(address)
+{
+    const {city, state} = address;                                                  //1. object destructuring
     const newAddress = {
-        city: address.city,
-        state: address.state,
+        city,                                                                       //2. object literal
+        state,                                                                      //2. object literal
         country: 'United States'
     };
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`);  //3. string/template literal
 }
 
-addressMaker({city:'Austin', state:'Texas'});
+addressMaker({city:'Austin', state:'Texas', });
