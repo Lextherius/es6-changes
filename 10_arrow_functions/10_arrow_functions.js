b59bb42d1736d28e88e215ffbf2abbc343cec508
Lextherius <alex@lexethos.com>
2019-04-17 18:19:46 +0800

    Arrow Function
    Vanilla code without using arrow function

---
+++ 10_arrow_functions/10_arrow_functions.js
@@ -0,0 +1,19 @@
+
+/*
+Arrow functions is a great way to eliminate some of the really unnecessary boiler plate of callback functions
+It comes with a lot of functionality and some gotchas
+*/
+
+function add(...nums){
+    let total = nums.reduce(function(x,y){
+
+        console.log(`x : ${x}`);
+        console.log(`y : ${y}`);
+
+        return x+y;
+    });
+
+    console.log(total);
+}
+
+add(4,5,7,8,12);
