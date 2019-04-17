1bfd1a178c579a96206400086156e8cc41b447a0
Lextherius <alex@lexethos.com>
2019-04-17 18:38:10 +0800

    Arrow Function
    Can be further simplify if we are doing a one line return in the arrow function.

--- 10_arrow_functions/10_arrow_functions.js
+++ 10_arrow_functions/10_arrow_functions.js
@@ -1,17 +1,10 @@
-
 /*
 Arrow functions is a great way to eliminate some of the really unnecessary boiler plate of callback functions
 It comes with a lot of functionality and some gotchas
 */
 
 function add(...nums){
-    let total = nums.reduce((x,y) => {
-
-        console.log(`x : ${x}`);
-        console.log(`y : ${y}`);
-
-        return x+y;
-    });
+    let total = nums.reduce((x, y) => x + y);
 
     console.log(total);
 }
