d6afae2ea61d027ab9a7057320e72db212da3301
Alex Yap <registrar@lextherius.com>
2019-04-19 17:37:19 +0800

    Let and Const
    Result
    Error, example is not defined

--- 13_let_and_const/13_let_and_const.js
+++ 13_let_and_const/13_let_and_const.js
@@ -1,18 +1,10 @@
 /*
-This example would not give you an error but instead show undefined.
-This is because of something call variable hoisting
-
-JavaScript Declarations are Hoisted
-In JavaScript, a variable can be declared after it has been used.
-In other words; a variable can be used before it has been declared.
-
-Hoisting is JavaScript's default behavior of moving all declarations to the top of 
-the current scope (to the top of the current script or the current function).
+Let is a stricter version of var and it uses block=scope
+Hence this example would give us an error.
 */
-
 if(false)
 {
-    var example = 5;
+    let example = 5;
 }
 
 console.log(example);
