5a231b9d3292b8f6742cc7452b0f033960fa521e
Alex Yap <registrar@lextherius.com>
2019-05-13 19:00:17 +0800

    Async and Await
    show case how to catch error of the failed fetch call

--- 18_async_await/18_async_await.js
+++ 18_async_await/18_async_await.js
@@ -1,11 +1,13 @@
 const fetch = require("node-fetch");
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20";
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 function getPokemons() {
     fetch(apiUrl)
     .then((r) => r.json())
     .then((json) => {
         console.log(json.results[0]);
+    }).catch((error) => {
+        console.log('Failed');
     });
 }
 
