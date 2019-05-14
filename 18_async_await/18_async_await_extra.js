bb4a2375d8dd87b2b48e97610f4fd3c24325d8cd
Alex Yap <registrar@lextherius.com>
2019-05-14 15:50:25 +0800

    Async and Await
    Refactor the code using async await

--- 18_async_await/18_async_await_extra.js
+++ 18_async_await/18_async_await_extra.js
@@ -1,16 +1,29 @@
 const fetch = require('node-fetch');
 
-function fetchStarshipsOwned(peopleId) {
-    return fetch(`https://swapi.co/api/people/${peopleId}/`)
-    .then(response => response.json())
-    .then(people => {
-        const promises = people.starships.map(url => {
-            fetch(url)
-            .then(response => response.json())
-            .then(starship => starship.name);
-        });
-        return Promise.all(promises);
-    });
+async function fetchStarshipsOwned(peopleId) {
+    const response = await fetch(`https://swapi.co/api/people/${peopleId}/`);
+    const people = await response.json();
+
+    const ships = [];
+    for(const shipLink of people.starships)
+    {
+        const response = await fetch(shipLink);
+        const ship = await response.json();
+        ships.push(ship.name);
+    }
+    
+    return ships;
+
+    // return fetch(`https://swapi.co/api/people/${peopleId}/`)
+    // .then(response => response.json())
+    // .then(people => {
+    //     const promises = people.starships.map(url => {
+    //         fetch(url)
+    //         .then(response => response.json())
+    //         .then(starship => starship.name);
+    //     });
+    //     return Promise.all(promises);
+    // });
 }
 
 const result = fetchStarshipsOwned(1);
